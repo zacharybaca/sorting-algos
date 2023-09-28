c59e28704afb0b99be775874d44f36991160095c
Zachary Baca <zachary.baca@msn.com>
2023-09-28 14:11:48 -0500

    Update sorting_algos.js

--- sorting_algos.js
+++ sorting_algos.js
@@ -119,14 +119,51 @@ console.log(mergeSort([2, 5, 10, 1, 0, 2]));
 
 //Quick Sort Function
 
-function quickSort(arr) {
-    // Choose an element called "the pivot", how that's done is up to the implementation.
-    // Take two variables to point left and right of the list excluding pivot.
-    // Left points to the low index.
-    // Right points to the high index.
-    // While value at left is less than pivot move right.
-    // While value at right is greater than pivot move left.
-    // If both step 5 and step 6 does not match, swap left and right.
-    // If left ≥ right, the point where they met is new pivot.
-    // Repeat, recursively calling this for smaller and smaller arrays.
+function quickSort(arr, indexLeft = 0, indexRight = arr.length - 1) {
+    // Base case: If there's only one or zero elements in the subarray, it's already sorted.
+    if (indexLeft >= indexRight) {
+        return arr;
+    }
+
+    // Choose a pivot (middle element in this case).
+    let pivotIndex = Math.floor((indexLeft + indexRight) / 2);
+    let pivot = arr[pivotIndex];
+
+    // Initialize pointers for partitioning.
+    let left = indexLeft;
+    let right = indexRight;
+
+    while (left <= right) {
+        // Find an element from the left that is greater than or equal to the pivot.
+        while (arr[left] < pivot) {
+            left++;
+        }
+
+        // Find an element from the right that is less than or equal to the pivot.
+        while (arr[right] > pivot) {
+            right--;
+        }
+
+        // If left pointer is less than or equal to the right pointer, swap elements.
+        if (left <= right) {
+            // Swap arr[left] and arr[right].
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // Move both pointers towards each other.
+            left++;
+            right--;
+        }
+    }
+
+    // Recursively sort the subarrays.
+    quickSort(arr, indexLeft, right);
+    quickSort(arr, left, indexRight);
+
+    return arr;
 }
+
+const result = quickSort([5, 7, 10, 2, 1, 0, 20]);
+console.log(result);
+
